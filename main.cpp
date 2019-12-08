539cd506b914af3ddfcb42e8382454a440e8c578
Jeremy Glebe <jeremyglebe@gmail.com>
2019-12-07 23:59:58 -0600

    tinkering with Termio functions

--- main.cpp
+++ main.cpp
@@ -3,13 +3,19 @@
 
 int main()
 {
+    Term::IO io;
     Cell test;
+    io << Term::clear;
     test.spawn();
     test.print();
     test.next();
     test.print();
     test.kill();
     test.print();
+    test.next();
+    test.print();
+    io << Term::Point(0,0);
+    test.spawn();
     test.next();
     test.print();
 }
